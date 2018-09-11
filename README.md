2858570eef54806da1e68df5295f0e1b5947c5ae
Elliot Shohet <elliot.shohet@gmail.com>
2018-09-11 16:52:54 -0700

    Move stuff again

--- README.md
+++
@@ -1,8 +0,0 @@
-# Reddit Cash
-
-For each post an ERC20 is created. ERC20s are automatically liquid, with the price determined by a curve (liquid token bonded curves).
-
-## Starting
-
-`npm run chain && truffle deploy && npm start`
-
--- migrations/2_deploy_scripts.js
+++
@@ -1,8 +0,0 @@
-const RedditCash = artifacts.require("RedditCash");
-const CuratedBondedCurve = artifacts.require("CuratedBondedCurve");
-
-
-module.exports = function(deployer) {
-  deployer.deploy(RedditCash);
-  deployer.deploy(CuratedBondedCurve);
-};
