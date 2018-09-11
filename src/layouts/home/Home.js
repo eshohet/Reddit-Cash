82c88f285fc5eecd73a847fc4d863a69a1dec151
Elliot Shohet <elliot.shohet@gmail.com>
2018-09-11 13:33:29 -0700

    Remove placeholders

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -78,14 +78,13 @@ class Home extends Component {
                         <FormGroup row>
                             <Label for="title" sm={2}>Title</Label>
                             <Col sm={10}>
-                                <Input type="text" name="title" id="title" placeholder="Post title"/>
+                                <Input type="text" name="title" id="title"/>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label for="postContents" sm={2}>Post Contents</Label>
                             <Col sm={10}>
-                                <textarea rows={10} cols={110} style={{width: '100%'}} id="postContents"
-                                          placeholder="Post contents"/>
+                                <textarea rows={10} cols={110} style={{width: '100%'}} id="postContents"/>
                             </Col>
                         </FormGroup>
                     </Form>
