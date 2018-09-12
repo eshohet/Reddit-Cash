c3953317aa4c92a86b13fdc3a0fec2861d13bb5c
Elliot Shohet <elliot.shohet@gmail.com>
2018-09-11 18:28:52 -0700

    Deploying again

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/" component={Home}/>
+        <Route path="/" component={Home}/>
       </div>
     );
   }
