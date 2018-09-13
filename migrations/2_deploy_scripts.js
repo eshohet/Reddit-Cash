92b7d0603d68143bd25e768a5c7a9c70e979bc7f
Elliot Shohet <elliot.shohet@gmail.com>
2018-09-13 15:28:14 -0700

    Getting ready for deploy

--- migrations/2_deploy_scripts.js
+++ migrations/2_deploy_scripts.js
@@ -2,7 +2,8 @@ const RedditCash = artifacts.require("RedditCash");
 const CuratedBondedCurve = artifacts.require("CuratedBondedCurve");
 
 
-module.exports = function(deployer) {
-  deployer.deploy(RedditCash);
-  deployer.deploy(CuratedBondedCurve);
+module.exports = function (deployer, network) {
+    deployer.deploy(RedditCash);
+    if (network === "development")
+        deployer.deploy(CuratedBondedCurve);
 };
--- truffle.js
+++ truffle.js
@@ -1,16 +1,23 @@
 module.exports = {
-  migrations_directory: "./migrations",
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+    migrations_directory: "./migrations",
+    networks: {
+        development: {
+            host: "localhost",
+            port: 7545,
+            network_id: "*" // Match any network id
+        },
+        ropsten: {
+            host: "localhost",
+            port: 8545,
+            network_id: 3,
+            skipDryRun: true,
+            from: "0xbef388e584897c4703f4417b8e5b7150ada53d96"
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 500
+        }
     }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 500
-    }
-  } 
 };
